7f88c2713c6258a3c6754aaf9900daae89641851
AnjaliPeter <74590192+AnjaliPeter@users.noreply.github.com>
2021-04-11 15:54:05 +0530

    Add files via upload

--- Frontend/src/app/design/design.component.ts
+++ Frontend/src/app/design/design.component.ts
@@ -1,1 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
 
+
+@Component({
+  selector: 'app-design',
+  templateUrl: './design.component.html',
+  styleUrls: ['./design.component.css']
+})
+export class DesignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    $("#btnradio1").attr({"class":"active"});
+  }
+
+  select(){
+    $("#btnradio1").attr({"class":"active"});
+    $("#btnradio2").attr({"class":"inactive"});
+  }
+
+  create(){
+    $("#btnradio2").attr({"class":"active"});
+    $("#btnradio1").attr({"class":"inactive"});
+  }
+
+}
