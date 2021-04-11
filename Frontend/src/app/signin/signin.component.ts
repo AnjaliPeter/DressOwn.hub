670384e657b95efe34f91f223bc66925c331e012
AnjaliPeter <74590192+AnjaliPeter@users.noreply.github.com>
2021-04-11 16:11:01 +0530

    Add files via upload

--- Frontend/src/app/signin/signin.component.ts
+++ Frontend/src/app/signin/signin.component.ts
@@ -1,1 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from '../account.service';
 
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+
+  constructor(private accountService:AccountService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  email:any;
+  password:any;
+  designation:any;
+  accntObj:any;
+
+  login(){
+    this.accountService.searchAccount(this.email)
+    .subscribe(data => {
+      this.accntObj = data;
+      if(this.accntObj!= null)
+      {
+        if(this.accntObj.password == this.password && this.accntObj.designation == this.designation)
+        {
+          alert("Hai "+this.accntObj.firstname+",  Welcome to DressOwn.hub !!!");
+          this.router.navigate(["/home"]);
+        }
+        else
+        {
+          alert("Wrong credentials, try again !!!");
+        }
+
+      }
+      else
+      {
+        alert("Wrong credentials, try again !!!");
+      }
+    });
+    
+  }
+
+}
