552610291044a8038b12c6c20c15415c2f90d06f
AnjaliPeter <74590192+AnjaliPeter@users.noreply.github.com>
2021-04-11 16:12:27 +0530

    Add files via upload

---
+++ Frontend/src/app/signup/account.model.ts
@@ -0,0 +1,13 @@
+export class AccountModel{
+
+    constructor(
+        public firstname : String,
+        public lastname : String,
+        public email : String,
+        public designation : String,
+        public purpose : String,
+        public password : String,
+        public phoneno : Number
+    ){}
+
+}
--- Frontend/src/app/signup/signup.component.ts
+++ Frontend/src/app/signup/signup.component.ts
@@ -1,1 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from '../account.service';
+import { AccountModel } from './account.model';
 
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor(private accountService:AccountService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  accountItem = new AccountModel("","","","","","",0);
+
+  accntObj:any;
+
+  addAccount()
+  {
+    var email = this.accountItem.email;
+    this.accountService.newAccount(this.accountItem)
+    .subscribe(data => {
+      this.accntObj = data;
+      alert(this.accntObj);
+    });
+    alert("Created a DressOwn.hub account successfully !!! You can signin to your account using the E-mail : "+email);
+    this.router.navigate(["/signin"]);
+  }
+
+
+}
